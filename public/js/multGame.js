9f7d0044bb9ba3df9369589335e46846ec76cc26
chlguswls6270 <59691666+chlguswls6270@users.noreply.github.com>
2024-08-07 14:27:08 +0900

    fixed WebSocket protocol to be wss when web is https

--- public/js/multGame.js
+++ public/js/multGame.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("DOMContentLoaded event fired");
     const room = window.location.pathname.split('/')[2];
     console.log("room number game.js: " + room);
-    const ws = new WebSocket(`ws://${location.host}/single-public?room=${room}`);
+    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const ws = new WebSocket(`${protocol}://${location.host}/single-public?room=${room}`);
     console.log("====chlguswls");
     const form = document.getElementById('userForm');
     const input = document.getElementById('inputField');
